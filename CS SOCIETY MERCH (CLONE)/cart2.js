842610a16c4e0d2bd33d65d6f7f11a6cecc6e3e7
Amrdanial_ <122141550+Some0ne11@users.noreply.github.com>
2023-10-29 13:05:03 +0800

    Add files via upload

---
+++ CS SOCIETY MERCH (CLONE)/cart2.js
@@ -0,0 +1,17 @@
+// Delivery Method Dropdown
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
